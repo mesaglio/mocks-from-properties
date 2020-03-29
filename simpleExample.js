94a518d8968c1776cbd074ee698f57393e896cb2
Juan Mesaglio <juan.mesaglio@gmail.com>
2020-03-29 11:15:05 -0300

    Update simpleExample.js

--- simpleExample.js
+++ simpleExample.js
@@ -1,8 +1,8 @@
 var res = require('./getResponses');
 
-var getPerson = res.getResponsFromConfig('./properties', 'getPerson');
-var certant = res.getSpecificRespons('./properties', 'getUser', 'certant');
-var comafi = res.getSpecificRespons('./properties', 'getUser', 'comafi');
+var getPerson = res.getResponsFromConfig('./properties', 'getPerson', 'http://localhost:9000/model');
+var certant = res.getSpecificRespons('./properties', 'getUser', 'http://localhost:9000/model', 'certant');
+var comafi = res.getSpecificRespons('./properties', 'getUser', 'http://localhost:9000/model', 'comafi');
 
 getPerson.then(x => console.log('\n GetPersona ' + JSON.stringify(x)));
 certant.then(x => console.log('\n Certant ' + JSON.stringify(x)));
